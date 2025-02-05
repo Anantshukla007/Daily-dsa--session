a68dde5fcfe95664e56510f902697c1727a2d3a9
Anantshukla007 <119282104+Anantshukla007@users.noreply.github.com>
2025-02-05 14:54:29 +0000

    some more problems

--- Hashing_and_prefixsum.cpp
+++ Hashing_and_prefixsum.cpp
@@ -729,3 +729,756 @@ int main(){
 }
 cout<<answer;
 return 0;
+
+
+
+//
+/*P0 : You are given an array of size “N”; all elements are 0 in it :) 
+You are given Q queries :-> [L,R] ; you have to add +1 to all the elements from [L,R] 
+
+-> After all queries are performed you have to print the final answer 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,Q,p;
+    cin>>n>>Q>>p;
+
+    int b[100001]={0};
+
+    for(int i=1;i<=Q;i++){
+
+        int L,R;
+
+        cin>>L>>R;
+
+        b[L]=b[L]+1;
+
+        if(R+1<=n){
+            b[R+1]=b[R+1]-1;
+        }
+    }
+
+    for(int i=1;i<=n;i++){
+        b[i]=b[i]+b[i-1];
+
+    }
+
+
+    for(int i=1;i<=p;i++){
+        int y;
+        cin>>y;
+        cout<<b[y]<<endl;
+    }
+
+    return 0;
+}
+
+
+
+/*
+
+
+> You are given an array of size “N” and two integers “x” and “y” 
+-> Array only consists of integers “x” and “y”
+-> Find the count of subarrays which have equal number of “x” and “y” 
+
+*/
+
+//when in any particular range lets say i to j then countofx[j]-countofx[i-1] will be equal to countofy[j]-countofy[i-1]
+// so this is a simple trick to solve this type of problems 
+
+
+#include<iostream>
+#include<vector>
+#include<unorderd_map>
+
+using namespace std;
+
+int main(){
+    int n,x,y;
+
+    cin>>n>>x>>y;
+
+    for(int i=1;i<=n;i++){
+        cin>>b[i];
+    }
+
+    long long count =0;
+    unordered_map<int,int>mp;
+
+    mp[0]=1;
+
+    int cx=0;
+    int cy=0;
+
+    for(int i=1;i<=n;i++){
+        if(b[i]==x){
+            cx++;
+        }
+        else{
+            cy++;
+        }
+        int d=cx-cy;
+        count+=mp[d];
+        mp[d]++;
+
+
+    }
+
+    cout<<count<<endl;
+    return 0;
+}
+
+
+//follow up what if we had three numbers x ,y and z;
+
+// the logic would remain same but we will need to check not two but three differnces to coexist in a range
+
+
+#include<bits/stdc++.h>
+using namesapce std;
+
+int main(){
+
+    int n,x,y,z;
+
+    cin>>n>>x>>y>>z;
+
+    vector<int>b(n+1);
+
+    for(int i=1;i<=n;i++){
+        cin>>b[i];
+
+    }
+
+
+    long long count =0;
+
+    map<pair<int,int>,int>mp;
+    mp[{0,0}]=1;
+
+    int cx=0;
+    int cy=0;
+    int cz=0;
+
+
+    for(int i=1;i<=n;i++){
+
+        if(b[i]==x){
+            cx++;
+        }
+        else if(b[i]==y){
+            cy++;
+        }
+        else{
+            cz++;
+        }
+
+        int d1=cy-cx;
+        int d2=cz-cy;
+
+        count+=mp[{d1,d2}];
+
+        mp[{d1,d2}]++;
+    }
+    cout<<count<<endl;
+    return 0;
+}
+
+
+//now the same question but wiht four numbers
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int solution(int n,int x,int y,int z,int w,int b){
+    int count =0;
+
+    map<tuple<int,int,int ,int>>mp;
+
+    mp[{0,0,0,0}]=1;
+
+    int cx=0;
+    int cy=0;
+    int cw=0;
+    int cb=0;
+    int cz=0;
+
+    for(int i=0;i<n;i++){
+
+
+        int val;
+        cin>>val;
+
+        if(val==x){
+            cx++;
+        }
+        else if(val==y){
+            cy++;
+        }
+        else if(val==z){
+            cz++;
+        }
+        else if(val==w){
+            cw++;
+        }
+        else{
+            cb++;
+        }
+
+
+        int d1=cy-cx;
+        int d2=cz-cy;
+        int d3= cw-cz;
+        int d4= cb-cw;
+
+        count+= mp[{d1,d2,d3,d4}];
+
+        mp[{d1,d2,d3,d4}];
+
+
+
+    }
+
+    return count;
+}
+
+/*Implement a simple meeting assistant. A list of strings, events[n], in the form "<person_name> <action> <start> <end>" is provided
+where person_name performs action from start to end, both inclusive. Times are formatted HH:MM. Find the earliest time in the day, from "00:00" to "23:59", when all people mentioned in at least one event are available for a meeting of k minutes. Report the answer as "HH:MM" or the string "-1" if it is not possible.*/
+
+
+/* the first thing about this question is that we need to conver the HH:MM totla minute  */
+
+
+#include<bits.stdc++.h>
+using namespace std;
+
+
+//converting then hh:MM format to total minutes
+int convert(string s){
+    int u1=int(s[0])-48;
+    int u2=int (s[1])-48;
+
+    int v=(u1*10+u5)*60;
+
+    u1=int(s[3])-48;
+    u5=int(s[4])-48;
+
+    v=v+(u1*10+u5)
+
+    return v;
+
+
+}
+
+
+void ck(int g){
+    int u=g/60;
+    int y=g%60;
+
+    if(u<=9){
+        cout<<"0";
+        cout<<u;
+    }
+
+    else{
+        cout<<u;
+    }
+
+    cout<<":";
+
+    if(y<=9){
+        cout<<"0";
+        cout<<y;
+    }
+    else{
+        cout<<y;
+
+    }
+
+    int main(){
+        int n;
+
+        cin>>n;
+
+        int k;
+        cin>>k;
+
+        int y[10000]={0};
+
+        for(int i=0;i<n;i++){
+
+            string a,b,v1,v5;
+
+            cin>>a;
+            cin>>b;
+            cin>>v1>>v5;
+
+            int t1=convert(v1);
+            int t5=convert(v5);
+
+            y[t1]++;
+            y[t5+1]--;
+        }
+        int i=1;
+
+        while(i<10000){
+            y[i]=y[i]+y[i-1];
+            i++;
+        }
+
+        int c=0;int g=0;
+
+        for(int i=0;i<10000;i++){
+
+            if(y[i]==0){
+                c++;
+            }
+            if(c==k){
+                ck(i-k+1);
+                g=1;
+                break;
+            }
+
+
+        }
+        else{
+            c=0;
+        }
+    }
+    if(g==0){
+        cout<<"-1";
+    }
+}
+
+
+
+/*
+
+Q. Find the count. of subarray of shortest length whose sum==k
+A = {10,5,2,7,1,9,8,7}   k = 15
+
+
+*/
+
+#include<bits/stdc++.h>
+using namesapce std;
+
+void subarray_problem(vector<int>arr,int k){
+
+    int mini=1e9;
+
+    int sum=0;
+    unorderd_map<int,int>mp;
+
+    mp[0]=-1;
+
+    for(int i=0;i<arr.size();i++){
+        sum+=arr[i];
+        if(mp.find(sum-k)!=mp.end()){
+            mini=min(mini,i-mp[sum-k]);
+
+        }
+
+        mp[sum]=i;
+    }
+
+    if(mini !=1e9){
+        int i=0;
+        int cnt=0;
+        int s=0;
+        int k1=mini;
+        while(j<arr.size()){
+            s+=arr[j];
+            if(j-i+1<k1){
+                j++;
+            }
+
+            else if(j-i+1==k1){
+                if(s==k){
+                    cnt++;
+                }
+                s-=arr[i];
+                i++;
+                j++;
+            }
+        }
+        cout<<cnt;
+
+    }
+
+
+}
+
+int main(){
+    vector<int>arr={5,46,465,65,65,6};
+    int k=5;
+    subarray_problem(arr,k);
+    return 0;
+}
+
+
+/*Task given in problem is to Find Number of Pairs in array A, i and j such that A[A[A[i]]] == A[A[A[j]]]
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n+1];
+    int i=1;
+    while(i<=n){
+        cin>>a[i];
+        i++;
+    }
+
+    unorderd_map<int,int>mp;
+
+    int count=0;
+    i=1;
+
+    while(i<=n){
+        int rhs=a[a[a[i]]];
+        int g=mp[rhs];
+
+        count=count+g;
+        int lhs=a[a[a[i]]];
+
+        mp[lhs]=mp[lhs]+1;
+        i++;
+    }
+
+    cout<<count;
+    return 0;
+}
+
+
+
+/*Maximum Sum Selection
+
+You are given an integer array A of size N. Your task is to select exactly B elements from either the left end or the right end of the array A in order to maximize the sum of the selected elements.
+
+Here are the details of the problem:
+
+You are given an integer array A of size N where 1 <= N <= 10^5.
+You need to select exactly B elements from either the left end or the right end of the array A.
+The goal is to find and return the maximum possible sum of elements you can pick.
+
+Input:
+
+An integer array A of size N where each element A[i] satisfies -103 <= A[i] <= 103.
+An integer B where 1 <= B <= N.
+Output:
+
+An integer representing the maximum possible sum of selected elements.
+
+[5, -2, 3, 1, 2], 3 -> 8*/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int fun(vector<int>a,int b){
+
+    int n=a.size();
+    int suff[n+1];
+
+    suff[n]=0;
+    suff[n-1]=a[n-1];
+    for(int i=n-2;i>=0;i--){
+        suff[i]=suff[i+1]+a[i];
+
+    }
+
+    int presum=0;
+    int ans=suff[n-b];
+
+    for(int i=0;i<b;i++){
+        presum=presum+a[i];
+        int suffsum=suff[n-b+(i+1)];
+        ans=max(ans,presum+suffsum);
+    }
+
+    return ans;
+}
+
+int main()
+{
+	vector<int>A={5, -2, 3,1,2};
+	int B=3;
+	cout<<solve(A,B);
+	return 0;
+}
+
+/*-> You are given string a and string b ; you have to output Yes ; if both of the strings can be made equal by doing some specific operations. 
+
+What are those operations?
+
+-> You can swap any even index element with any other even index element of same string 
+*/
+
+
+bool check(string x,string y){
+    int i=0;
+
+    unorderd_map<char,int>mp1,mp2,mp3,mp4;
+
+    while(i<x.size()){
+        char g=x[i];
+
+        if(i%2==0){
+            mp1[g]++;
+        }
+        else{
+            mp2[g]++;
+        }
+        i++;
+
+    }
+    i=0;
+
+    while(i<y.size()){
+        char g=y[i];
+
+        if(i%2==0){
+            mp3[i]++;
+        }
+        else{
+            mp4[g]++;
+        }
+        i++;
+    }
+
+    if(mp1==mp3 && mp2== mp4){
+        return true;
+    }
+    return false;
+    
+}
+
+/*Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
+
+0 <= i, j, k, l < n
+nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0*/
+
+
+
+int solve(vector<int>nums1,vector<int>numd2;vector<int>nums3,vector<int>nums4){
+
+    map<int,int>mp;
+
+    for(int k:nums3){
+        for(int l:nums4){
+            mp[k+l]++;
+        }
+
+
+    }
+
+    int count=0;
+
+    for(int i:nums1){
+        for(int j:nums2){
+            count+=mp[-(i+j)];
+            
+        }
+    }
+
+    return count;
+}
+
+
+/*Problem.-> Given an array of size “N”; find the number of triplets; such that A[i] >A[j]< A[k] such that i < j < k ; 
+*/
+
+
+#include<bits/stdc++.h>
+using namsepace std;
+int main(){
+    int n;
+    cin>>n;
+
+    int b[n+1];
+
+    int pre[n+1]={0};
+
+    int suf[n+1]={0};
+
+    pre[1]=0;
+
+    for(int i=1;i<=n;i++){
+        cin>>b[i];
+    }
+
+    int j=2;
+    while(j<=n){
+
+        int c=0;
+
+        int i=1;
+        while(i<=j-1){
+            if(b[i]>b[j]){
+                c++;
+            }
+            i++;
+
+
+
+        }
+
+        pre[j]=t;
+        j++;
+
+
+    }
+
+    suff[n]=0;
+
+
+
+
+    int k=n-1;
+    while(k>=1){
+
+        int c=0;
+        int l=k+1;
+        while(l<=n){
+            if(b[k]>b[l]){
+                c++;
+            }
+            l++;
+        }
+        suff[k]=c;
+        k--;
+    }
+
+    int c=0;
+
+    int j=1;
+
+    while(j<=n){
+        int k=j+1;
+
+        while(k<=n){
+            if(b[j]<b[k]){
+                c=c+pre[j]*suf[k];
+            }
+
+            k++;
+        }
+        j++;
+    }
+    cout<<c;
+    return 0;
+    
+}
+
+
+
+/*
+Given 5 arrays :- A,B,C,D,E  all of size “N” 
+
+Element in the array can range from [-10^9……….10^9]
+
+Find the number of tuplets (i,j,k,l,m) such that  - 
+
+a[i] + b[j] + c[k] + d[l] + e[m] = 0
+
+A = [2 5]
+B = [3 8]
+C = [-5 8]
+D = [5 10]
+E = [-10 100] 
+
+Output - 1 (2,5,-5,10,-10) 
+
+
+
+*/
+
+
+#inlcude<iostream>
+#include<unorderd_map>
+
+using namespace std;
+
+int main(){
+
+
+    int n=2;
+    int a[] = {2, 5};
+    int b[] = {3, 8};
+    int c[] = {-5, 8};
+    int d[] = {5, 10};
+    int e[] = {-10, 100};
+
+    unorderd_map<int,int>mp;
+
+    for(int i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            int sum=d[l]+e[m];
+            mp[sum]++;
+        }
+    }
+
+    int count=0;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++){
+                int reduced_sum=-(a[i]+b[j]+c[k]);
+                count+=mp[reduced_sum];
+            }
+        }
+    }
+
+    cout<<count<<endl;
+}
+
+
+/*Input:
+N = 3
+X = {"abc", "ade", "bbc"}
+Output: [3, 0, 0]
+Explanation:
+For String "abc"(i 1), the subsequent strings are "ade" and "bbc". The matching characters of "abc" with "ade" is 1 which is 'a' at index 1 and the matching characters of "abc" with. "bbc" is 2 which are 'b' and 'c' at index 2 and 3. The answer for "abc" is 1+2=3
+For String "ade" (i = 2). the subsequent string is "bbc". The matching characters of "ade" with "bbc' is 0. The answer for "ade" is 0.
+For String "bbc"(i answer for "bbc" is 0. 3), there are no subsequent strings. The
+We can note that the answer for the last string in the input array will always be 0 as there are no subsequent strings.*/
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+vector<int>matching(int n, vector<string>x){
+    vector<vector<int>>g(20000+5,vector<int>(28,0));
+
+    vector<int>p(n,0);
+
+
+    for(int i=n-1;i>=0;i--){
+        string u=x[i];
+
+        int c=0;
+        int d=u.size();
+        for(int j=0;j<d;j++){
+            int y=int(u[j])-97;
+
+            c=c+g[j][y];
+        }
+
+        p[i]=c;
+    }
+
+    return p;
+}
+
